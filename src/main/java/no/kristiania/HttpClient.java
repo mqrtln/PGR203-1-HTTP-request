af0283fff22a6c8e4a2f5b5ba927191868445226
Martin Olaussen <maol049@EGMS.no>
2021-09-07 16:57:07 +0200

    shouldGetFailureResponseCode(red)

--- src/main/java/no/kristiania/HttpClient.java
+++ src/main/java/no/kristiania/HttpClient.java
@@ -7,20 +7,28 @@ import java.nio.charset.StandardCharsets;
 
 
 public class HttpClient {
-   public static void main(String[] args) throws IOException {
-       Socket socket = new Socket("httpbin.org", 80);
-       String request = """
+    public HttpClient(String s, int i, String s1) throws IOException {
+
+        Socket socket = new Socket("httpbin.org", 80);
+        String request = """
                GET /html HTTP/1.1\r
                Host: httpbin.org\r
                \r
                """;
 
-       socket.getOutputStream().write(request.getBytes());
+        socket.getOutputStream().write(request.getBytes());
 
-       InputStream in = socket.getInputStream();
-       int c;
-       while ((c = in.read()) != -1){
-           System.out.print((char) c);
-       }
+        InputStream in = socket.getInputStream();
+        int c;
+        while ((c = in.read()) != -1){
+            System.out.print((char) c);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
    }
+
+    public int getStatusCode() {
+        return 200;
+    }
 }
