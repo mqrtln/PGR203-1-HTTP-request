b3b6e571bf3b5ac0b3f5cf024aedf3ce07a4cfb4
Martin Olaussen <maol049@EGMS.no>
2021-09-07 16:47:26 +0200

    HttpClient gets the body and header from httpbin.org

--- src/main/java/no/kristiania/HttpClient.java
+++ src/main/java/no/kristiania/HttpClient.java
@@ -1,12 +1,26 @@
 package no.kristiania;
 
-public class HttpClient {
-    public static String dummyReturnNull(){
-        //test
-        return null;
-    }
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
-    public static int return10() {
-        return 10;
-    }
+
+public class HttpClient {
+   public static void main(String[] args) throws IOException {
+       Socket socket = new Socket("httpbin.org", 80);
+       String request = """
+               GET /html HTTP/1.1\r
+               Host: httpbin.org\r
+               \r
+               """;
+
+       socket.getOutputStream().write(request.getBytes());
+
+       InputStream in = socket.getInputStream();
+       int c;
+       while ((c = in.read()) != -1){
+           System.out.print((char) c);
+       }
+   }
 }
--- src/test/java/no/kristiania/HttpClientTest.java
+++ src/test/java/no/kristiania/HttpClientTest.java
@@ -7,13 +7,4 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpClientTest {
 
-    @Test
-    void dummyShouldReturnNull(){
-        assertEquals(null, HttpClient.dummyReturnNull());
-    }
-
-    @Test
-    void dummyShouldReturn10(){
-        assertEquals(10, HttpClient.return10());
-    }
 }
